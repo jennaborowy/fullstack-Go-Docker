f31b45b029f8de99109a30750023c69e585ffec1
jennaborowy <jenna.borowy@gmail.com>
2025-10-03 12:57:31 -0400

    item things now consider created and updated timestamps

--- backend/models/item.go
+++ backend/models/item.go
@@ -3,9 +3,21 @@ package models
 import "time"
 
 type Item struct {
-	ID      int
-	Title   string
-	Date    time.Time
-	Content string
-	ListID  int
+	ID        int
+	Title     string
+	Date      time.Time
+	Content   string
+	ListID    int
+	CreatedAt time.Time
+	UpdatedAt time.Time
+}
+
+// NewItem creates a new item
+func NewItem(title string, date time.Time, content string, listID int) *Item {
+	return &Item{
+		Title:   title,
+		Date:    date,
+		Content: content,
+		ListID:  listID,
+	}
 }
--- backend/repository/item_repository.go
+++ backend/repository/item_repository.go
@@ -21,7 +21,7 @@ func NewItemRepository(db *sql.DB) *ItemRepository {
 
 // GetAll retrieves all existing items from database
 func (r *ItemRepository) GetAll() ([]models.Item, error) {
-	rows, err := r.db.Query("SELECT id, title, date, content FROM items")
+	rows, err := r.db.Query("SELECT id, title, date, content, list_id, created_at, updated_at FROM items")
 	if err != nil {
 		return nil, err
 	}
@@ -30,7 +30,7 @@ func (r *ItemRepository) GetAll() ([]models.Item, error) {
 	var items []models.Item
 	for rows.Next() {
 		var item models.Item
-		if err := rows.Scan(&item.ID, &item.Title, &item.Date, &item.Content); err != nil {
+		if err := rows.Scan(&item.ID, &item.Title, &item.Date, &item.Content, &item.ListID, &item.CreatedAt, &item.UpdatedAt); err != nil {
 			return nil, err
 		}
 		items = append(items, item)
@@ -41,10 +41,10 @@ func (r *ItemRepository) GetAll() ([]models.Item, error) {
 
 // GetByID retrieves a single item by its ID
 func (r *ItemRepository) GetByID(id int) (*models.Item, error) {
-	row := r.db.QueryRow("SELECT id, title, date, content, list_id FROM items WHERE id = ?", id)
+	row := r.db.QueryRow("SELECT id, title, date, content, list_id, created_at, updated_at FROM items WHERE id = ?", id)
 
 	var item models.Item
-	err := row.Scan(&item.ID, &item.Title, &item.Date, &item.Content, &item.ListID)
+	err := row.Scan(&item.ID, &item.Title, &item.Date, &item.Content, &item.ListID, &item.CreatedAt, &item.UpdatedAt)
 	if err != nil {
 		if err == sql.ErrNoRows {
 			return nil, fmt.Errorf("item not found")
@@ -57,9 +57,9 @@ func (r *ItemRepository) GetByID(id int) (*models.Item, error) {
 
 // GetItemsByListID gets an item by the list it is in
 func (r *ItemRepository) GetByListID(listID int) (*models.Item, error) {
-	row := r.db.QueryRow("SELECT id, title, date, content, list_id FROM items WHERE list_id = ?", listID)
+	row := r.db.QueryRow("SELECT id, title, date, content, list_id, created_at, updated_at FROM items WHERE list_id = ?", listID)
 	var item models.Item
-	err := row.Scan(&item.ID, &item.Title, &item.Date, &item.Content, &item.ListID)
+	err := row.Scan(&item.ID, &item.Title, &item.Date, &item.Content, &item.ListID, &item.CreatedAt, &item.UpdatedAt)
 	if err != nil {
 		if err == sql.ErrNoRows {
 			return nil, fmt.Errorf("item not found")
@@ -91,7 +91,7 @@ func (r *ItemRepository) DeleteItemByID(id int) error {
 
 // CreateItem creates a new item with title, date, and content
 func (r *ItemRepository) CreateItem(title string, date time.Time, content string, listID int) (int64, error) {
-	res, err := r.db.Exec("INSERT INTO items (title, date, content, list_id) VALUES (?, ?, ?, ?)", title, date, content, listID)
+	res, err := r.db.Exec("INSERT INTO items (title, date, content, list_id, created_at, updated_at) VALUES (?, ?, ?, ?, ?, ?)", title, date, content, listID, time.Now(), time.Now())
 	if err != nil {
 		return 0, fmt.Errorf("failed to create new item: %w", err)
 	}
@@ -106,7 +106,7 @@ func (r *ItemRepository) CreateItem(title string, date time.Time, content string
 
 // UpdateItem updates an item's title, date, and/or content
 func (r *ItemRepository) UpdateItem(id int, title string, date time.Time, content string) error {
-	res, err := r.db.Exec("UPDATE items Set title = ?, date = ?, content = ? WHERE id = ?", title, date, content, id)
+	res, err := r.db.Exec("UPDATE items Set title = ?, date = ?, content = ?, updated_at = ? WHERE id = ?", title, date, content, time.Now(), id)
 	if err != nil {
 		return fmt.Errorf("could not update item: %w", err)
 	}
